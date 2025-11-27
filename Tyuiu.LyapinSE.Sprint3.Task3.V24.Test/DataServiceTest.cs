4bfd72e30d7858ddf0f5df59b8ab3de2046c2b4b
Zeron <dotdot20072607@gmail.com>
2025-11-28 02:09:31 +0500

    Task3

---
+++ Tyuiu.LyapinSE.Sprint3.Task3.V24.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.LyapinSE.Sprint3.Task3.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
