3656631d4c36959671d4049d265035dcdad999ba
Zeron <dotdot20072607@gmail.com>
2025-11-28 02:37:21 +0500

    Task5

---
+++ Tyuiu.LyapinSE.Sprint3.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.LyapinSE.Sprint3.Task5.V27.Lib
+{
+    public class DataService : ISprint3Task4V27
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 0;
+            for (int i = startValue; i < stopValue; i++)
+            {
+                for (int k = startValue; k < stopValue; k++)
+                {
+                    res += Math.Pow(k, 2) / Math.Cos(k);
+                }
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
