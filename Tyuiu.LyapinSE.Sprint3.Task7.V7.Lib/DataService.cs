4b47841a7acf47e367dfbb6c955adf44d0734a72
Zeron <dotdot20072607@gmail.com>
2025-11-28 03:45:47 +0500

    Task7

---
+++ Tyuiu.LyapinSE.Sprint3.Task7.V7.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.LyapinSE.Sprint3.Task7.V7.Lib
+{
+    public class DataService : ISprint3Task7V7
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray = new double[stopValue - startValue];
+
+            int index = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x + 2 == 0)
+                {
+                    valueArray[index] = 0;
+                    continue;
+                }
+                valueArray[index] = Math.Cos(x) + Math.Cos(x) / x + 2 - 3 * x;
+                index++;
+            }
+            return valueArray;
+        }
+    }
+}
